2ce903a8b5a3fea3e206922e6916b087925b5daf
mejaman <mrjaman@gmail.com>
2024-11-07 17:30:53 +0600

    This is my first commits - add - more changing

--- RavitAddinBootcamp_OrientationEJ1/cmdSkills01.cs
+++ RavitAddinBootcamp_OrientationEJ1/cmdSkills01.cs
@@ -12,7 +12,7 @@
 
             // Your Module 01 Skills code goes here
             // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 01 Skills", "Got Here! Now changing Let see");
+            TaskDialog.Show("Module 01 Skills", "Got Here! Now changing Let see - More changing");
 
 
             return Result.Succeeded;

b4f3e55dd0316f9880a352f14883ae3d2a1ed0ca
mejaman <mrjaman@gmail.com>
2024-11-09 00:48:57 +0600

    MyFinal update on cmdChallange01.cs

--- RavitAddinBootcamp_OrientationEJ1/cmdChallenge01_old.cs
+++ RavitAddinBootcamp_OrientationEJ1/cmdChallenge01_old.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 namespace RavitAddinBootcamp_OrientationEJ1
 {
     [Transaction(TransactionMode.Manual)]
-    public class cmdChallenge01 : IExternalCommand
+    public class cmdChallenge01_old : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -19,7 +19,7 @@ namespace RavitAddinBootcamp_OrientationEJ1
 
             // Your Module 01 Challenge code goes here
             // Delete the TaskDialog below and add your code
-            TaskDialog.Show("Module 01 Challenge", "cmd Challange 01!- Lets see");
+            TaskDialog.Show("Module 01 Challenge", "Coming Soon!- Lets see");
 
             // declare my variables
 
@@ -31,81 +31,39 @@ namespace RavitAddinBootcamp_OrientationEJ1
             int buzzCount = 0;
             int fizzBuzzCount = 0;
 
-            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
-            collector1.OfClass(typeof(ViewFamilyType));
-
-            ViewFamilyType floorPlanVFT = null;
-            foreach (ViewFamilyType curVFT in collector1)
-            {
-                if (curVFT.ViewFamily == ViewFamily.FloorPlan)
-                {
-                    floorPlanVFT = curVFT;
-                }
-            }
-
-          
-            ViewFamilyType ceilingPlanVFT = null;
-            foreach (ViewFamilyType curVFT in collector1)
-            {
-                if (curVFT.ViewFamily == ViewFamily.CeilingPlan)
-                {
-                    ceilingPlanVFT = curVFT;
-                }
-            }
-
-            // get a title block type
-            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
-            collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
-            collector2.WhereElementIsElementType();
 
 
-            
 
-
-            for (int i=1; i<= myVariable250; i++)
+            for (int i = 1; i <= 250; i++)
             {
 
                 //create a level with i
-                Level newLevel = Level.Create(doc, myStartingEleV);
-                newLevel.Name = "My new level" + i;
-                myStartingEleV += myFloorHeight;
+                // create a transaction to lock the model
+
 
                 // create a floor level 
-
-
+                Level newLevel = Level.Create(doc, i);
+                newLevel.Name = "My new level" + i;   //******Need to add level value (thinking to convert i value to string
+            
                 // check divisible
 
-
-                if ((i % 3) == 0 && (i % 5) == 0)
-                {
-                    //creat sheet and name "FIZZBUZZ"
-                    fizzBuzzCount++;
-
-                    // create a floor plan view
-                    ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
-                    newFloorPlan.Name = "FIZZBUZZ_" + fizzBuzzCount;
-
-                    // create a sheet
-                    ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElementId());
-                    newSheet.Name = "FIZZBUZZ_" + fizzBuzzCount;
-                    newSheet.SheetNumber = "A10" + fizzBuzzCount;
-
-                    // create a viewport
-                    // first create a point
-                    XYZ insPoint = new XYZ();
-                    XYZ insPoint2 = new XYZ(1, .6, 1);
-                    //XYZ insPoint2 = new XYZ(1, 0.5, (myStartingEleV - myFloorHeight));
-
-                    Viewport newViewport = Viewport.Create(doc, newSheet.Id, newFloorPlan.Id, insPoint2);
-
-                }
-
-                                
-                else if (i % 3 == 0)
+                levelRemainder = i % 3;
+                if (levelRemainder == 0)
                 {
                     //creat floor plan and name "FIZZ"
                     fizzCount++;
-                   
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType floorPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.FloorPlan)
+                        {
+                            floorPlanVFT = curVFT;
+                        }
+                    }
+
                     // create a floor plan view
                     ViewPlan newFloorPlan = ViewPlan.Create(doc, floorPlanVFT.Id, newLevel.Id);
                     newFloorPlan.Name = "FIZZ - " + fizzCount;
@@ -113,14 +71,24 @@ namespace RavitAddinBootcamp_OrientationEJ1
                 }
 
 
-               
-                else if (i % 5 == 0)
+                levelRemainder = i % 5;
+                if (levelRemainder == 0)
                 {
                     //creat ceiling plan and name "BUZZ"
 
                     // create a floor plan view
                     buzzCount++;
-                  
+                    FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+                    collector1.OfClass(typeof(ViewFamilyType));
+
+                    ViewFamilyType ceilingPlanVFT = null;
+                    foreach (ViewFamilyType curVFT in collector1)
+                    {
+                        if (curVFT.ViewFamily == ViewFamily.CeilingPlan)
+                        {
+                            ceilingPlanVFT = curVFT;
+                        }
+                    }
 
                     // create a ceiling plan view
                     ViewPlan newCeilingPlan = ViewPlan.Create(doc, ceilingPlanVFT.Id, newLevel.Id);
@@ -130,7 +98,23 @@ namespace RavitAddinBootcamp_OrientationEJ1
 
 
                 
-               
+                if ((i%3) == 0 && (i%5)==0)
+                {
+                    //creat sheet and name "FIZZBUZZ"
+                    fizzBuzzCount++;
+                    // get a title block type
+                    FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+                    collector2.OfCategory(BuiltInCategory.OST_TitleBlocks);
+                    collector2.WhereElementIsElementType();
+
+                    // create a sheet
+                    ViewSheet newSheet = ViewSheet.Create(doc, collector2.FirstElementId());
+                    newSheet.Name = "FIZZBUZZ_"+fizzBuzzCount;
+                    newSheet.SheetNumber = "A10"+fizzBuzzCount;
+
+                   
+
+                }
 
                 
             }
